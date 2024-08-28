ee29d6fcaef7c0c9c7b59258be7b7130d6b2e635
NidhiKJha <menidhikjha@gmail.com>
2024-08-28 15:29:32 +0100

    added edit networks functionality

--- ui/components/multichain/edit-networks-modal/edit-networks-modal.js
+++ ui/components/multichain/edit-networks-modal/edit-networks-modal.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { TextVariant } from '../../../helpers/constants/design-system';
 import { useI18nContext } from '../../../hooks/useI18nContext';
-import { getNonTestNetworks, getPermittedChainsByOrigin, getTestNetworks } from '../../../selectors';
+import { getNonTestNetworks, getOriginOfCurrentTab, getPermittedChainsByOrigin, getTestNetworks } from '../../../selectors';
 import {
   Modal,
   ModalOverlay,
@@ -12,15 +12,21 @@ import {
   Checkbox,
   Text,
   Box,
+  ModalFooter,
+  ButtonPrimary,
+  ButtonPrimarySize,
 } from '../../component-library';
 import { NetworkListItem } from '..';
+import { grantPermittedChains, removePermittedAccount, removePermittedChain } from '../../../store/actions';
 
 export const EditNetworksModal = ({ onClose }) => {
   const t = useI18nContext();
+  const dispatch = useDispatch();
   const nonTestNetworks = useSelector(getNonTestNetworks);
   const testNetworks = useSelector(getTestNetworks);
   const chains = useSelector(getPermittedChainsByOrigin);
   const permittedChains = Object.values(chains);
+  const activeTabOrigin = useSelector(getOriginOfCurrentTab);
   const flattenedPermittedChains = permittedChains.flat();
   const [selectedChains, setSelectedChains] = useState(
     flattenedPermittedChains,
@@ -39,9 +45,36 @@ export const EditNetworksModal = ({ onClose }) => {
         (_item, idx) => idx !== index,
       );
     }
-    console.log(newSelectedChains, 'ggg');
     setSelectedChains(newSelectedChains);
   };
+    const managePermittedChains = (
+      selectedChains,
+      flattenedPermittedChains,
+      activeTabOrigin,
+    ) => {
+      if (!Array.isArray(selectedChains)) {
+        console.error('selectedChains is not an array:', selectedChains);
+        selectedChains = [];
+      }
+
+      const newElements = selectedChains.filter(
+        (chain) => !flattenedPermittedChains.includes(chain),
+      );
+
+      dispatch(grantPermittedChains(activeTabOrigin, newElements));
+
+      const removedElements = flattenedPermittedChains.filter(
+        (chain) => !selectedChains.includes(chain),
+      );
+
+      console.log(removedElements, newElements, 'remo');
+
+      // Dispatch removePermittedChains for each removed element
+      removedElements.forEach((chain) => {
+        const selectedChain = [chain];
+        dispatch(removePermittedChain(activeTabOrigin, selectedChain));
+      });
+    };
   return (
     <Modal
       isOpen
@@ -98,6 +131,38 @@ export const EditNetworksModal = ({ onClose }) => {
             showEndAccessory={false}
           />
         ))}
+        <ModalFooter>
+          {selectedChains.length === 0 ? (
+            <ButtonPrimary
+              data-testid="disconnect-chains-button"
+              onClick={() => {
+                // disconnectAllAccounts();
+                onClose();
+              }}
+              size={ButtonPrimarySize.Lg}
+              block
+              danger
+            >
+              {t('disconnect')}
+            </ButtonPrimary>
+          ) : (
+            <ButtonPrimary
+              data-testid="connect-more-accounts-button"
+              onClick={() => {
+                managePermittedChains(
+                  selectedChains,
+                  flattenedPermittedChains,
+                  activeTabOrigin,
+                );
+                onClose();
+              }}
+              size={ButtonPrimarySize.Lg}
+              block
+            >
+              {t('confirm')}
+            </ButtonPrimary>
+          )}
+        </ModalFooter>
       </ModalContent>
     </Modal>
   );
